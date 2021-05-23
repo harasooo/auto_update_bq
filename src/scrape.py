749ed131d209a447bd29e9064c4299698dbf3e3b
harasooo <haraso@jxpress.net>
2021-05-24 01:52:33 +0900

    途中まで

---
+++ src/scrape.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from typing import List
+import re
+import requests
+import pandas as pd
+from pandas import Series, DataFrame
+import datetime
+from datetime import date, timedelta
+import base64
+from google.cloud import bigquery

cc4c35d72f2643ea9b561bb2631fd5d2d4880f80
harasoooo <soichiro.basuketo@gmail.com>
2021-07-25 16:16:13 +0900

    bug fix

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from config import PROJECT_ID, TABLE_ID, CLIENT, CREDENTIALS, SCHEMA
 from scrape import get_exist_race_list, get_race_list, get_table_list, write_bq
 import warnings
 
-warnings.simplefilter('ignore')
+warnings.simplefilter("ignore")
 
 
 def main():
     exist_race_list = get_exist_race_list(CLIENT, TABLE_ID)
     race_list = get_race_list()
     df = get_table_list(race_list, exist_race_list)
-    if df:
+    if len(df) > 0:
         write_bq(df, TABLE_ID, PROJECT_ID, CREDENTIALS, SCHEMA)
